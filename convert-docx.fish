62ae6ed8660744fb082774b32752bbae8b43d940
fiatjaf <fiatjaf@gmail.com>
2017-06-24 14:38:35 +0000

    getting git date from .docx and setting it on generated .html.

--- convert-docx.fish
+++ convert-docx.fish
@@ -1,4 +1,9 @@
 for docx in (find . -name "*.docx")
+  # generate an html file from the .docx
   set html5 (string split '.docx' "$docx" | tail -n 2 | head -n 1).html
   pandoc -f docx -t html5 -o "$html5" "$docx"
+
+  # set the generated document date to be equal to the .docx date
+  set gitdate (git log --pretty=format:%aI -- "$docx" | tail -n 1)
+  touch -d "$gitdate" "$html5"
 end

3bb076e159d71784ec36bfc9d14246fad70212be
fiatjaf <fiatjaf@gmail.com>
2017-07-30 13:15:04 +0000

    coisas live preview.

--- coisas.js
+++ coisas.js
@@ -31,3 +31,43 @@ window.load(
     })
   }
 })
+
+// react-site exports a lot of pre-required modules, including the `require()`
+// functions, so this will work.
+window.load('https://rawgit.com/fiatjaf/levypla/gh-pages/bundle.js')
+  .then(() => {
+    window.module = {exports: null}
+    return window.load('https://rawgit.com/fiatjaf/levypla/master/body.js')
+  })
+  .then(() => {
+    window.Body = window.module.exports
+  })
+
+window.coisas.canPreview = (_, ext) => ext === 'html' || ext === 'md'
+window.coisas.generatePreview = (el, {
+  path,
+  name,
+  ext,
+  mime,
+  content,
+  metadata,
+  slug,
+  tree,
+  edited
+}) => {
+  el.style.padding = '20px'
+  let sh = el.attachShadow({mode: 'open'})
+
+  let h = require('react-hyperscript')
+  let render = require('react-dom').render
+
+  var Component = require('wrap.js')({
+    filename: name,
+    pathname: path,
+    title: metadata.title,
+    summary: metadata.summary,
+    date: metadata.date
+  }, content)
+
+  render(h(Component), sh)
+}
